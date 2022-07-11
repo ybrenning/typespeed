2d49f63a65ccb54f08258791ec0359e5e73fba71
ybrenning <yannickbrenning2@gmail.com>
2022-07-11 22:44:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Typespeed
+
+This is a simple console game that lets you practice your touch typing on a variety of \
+sentences ranging from around 80-120 words each. You can finish the game by completing \
+the text or stop at any time by pressing `ENTER`. At the end, the game lets you view \
+basic statistics about your typing such as your speed in WPM (gross words per minute) and your accuracy percentage.
---
+++ typespeed.c
@@ -0,0 +1,133 @@
+#include <windows.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include "sentences.h"
+
+#define ARRAY_SIZE(arr) sizeof(arr) / sizeof(arr[0])
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+typedef struct Stats {
+    double wpm;
+    double accuracy;
+} Stats;
+
+char *select_sentence() {
+    srand(time(NULL));
+    size_t total = ARRAY_SIZE(sentences);
+    return (sentences[rand() % total]);
+}
+
+bool compare_letters(char input, char solution) {
+    return (input == solution ? true : false);
+}
+
+char take_input() {
+    char c = _getch();
+    return c;
+}
+
+Stats game_loop() {
+    bool flag = false;
+    clock_t start = clock();
+
+    Stats current_stats = {
+        0.0, 100.0
+    };
+
+    char current_input;
+    char *sentence = select_sentence();
+    size_t correct = 0;
+    size_t incorrect = 0;
+
+    printf("\n\n%s\n\n", sentence);
+
+    Sleep(1500);
+    printf("Starting... 3...");
+    Sleep(1000);
+    printf(" 2...");
+    Sleep(1000);
+    printf(" 1...\n\n");
+    Sleep(1000);
+
+    int i = 0;
+    while (sentence[i] != '\0') {
+        current_input = take_input();
+        if (current_input == '\n' || current_input == '\r') {
+            break;
+        }
+
+        if (current_input == 8) {
+            if (i != 0) {
+                printf("\b");
+                --i;
+            } 
+
+            continue;
+        }
+
+        if (compare_letters(current_input, sentence[i])) {
+            ++correct;
+            printf(ANSI_COLOR_GREEN);
+            printf("%c", current_input);
+        } else {
+            ++incorrect;
+            printf(ANSI_COLOR_RED);
+            printf("%c", current_input);
+            printf(ANSI_COLOR_RESET);
+        }
+
+        ++i;
+        printf(ANSI_COLOR_RESET);
+    }
+
+    current_stats.accuracy = (correct + incorrect == 0) ? 0.0 : (double) correct / ((double) (correct + incorrect)) * 100;
+
+    clock_t end = clock();
+    double minutes = ((double) (end - start) / CLOCKS_PER_SEC) / 60;
+
+    current_stats.wpm = ((double) (correct + incorrect) / 5) / minutes;
+
+    return current_stats;
+}
+
+int main() {
+    printf(
+        "_______   _______ _____   _____ _____ _____ _____  \n"
+        "|_   _\\ \\ / / ___ \\  ___| |_   _|  ___/  ___|_   _|\n"
+        "  | |  \\ V /| |_/ / |__     | | | |__ \\ `--.  | | \n"  
+        "  | |   \\ / |  __/|  __|    | | |  __| `--. \\ | | \n"  
+        "  | |   | | | |   | |___    | | | |___/\\__/ / | | \n"
+        "  \\_/   \\_/ \\_|   \\____/    \\_/ \\____/\\____/  \\_/  \n\n\n"
+    );
+
+    printf("Press ENTER to begin typing :^)\n");
+    char input = getche();
+    if (input == '\n' || input == '\r') {
+        Stats game_stats = game_loop();
+        printf("\n\n=====\n");
+
+        printf("Accuracy: %.2f%%\n", game_stats.accuracy);
+        printf("WPM: %.2f\n", game_stats.wpm);
+
+        printf("Score: ");
+        printf(ANSI_COLOR_YELLOW);
+        printf("%.2f", game_stats.accuracy * game_stats.wpm);
+
+        printf(ANSI_COLOR_RESET);
+        printf("\n=====\n\n\n");
+    }
+
+    return 0;
+}
